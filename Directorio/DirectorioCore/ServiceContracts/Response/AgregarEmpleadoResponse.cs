07d75c5271329d642eec29462b8ffd6ad9a63962
lazuk182 <lazgonmon@gmail.com>
2016-09-27 09:00:37 -0500

    Clase faltante de response en service contracts

---
+++ Directorio/DirectorioCore/ServiceContracts/Response/AgregarEmpleadoResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectorioCore.ServiceContracts.Response
+{
+    public class AgregarEmpleadoResponse
+    {
+        public AgregarEmpleadoResponse()
+        {
+            Message = string.Empty;
+            Success = string.Empty;
+        }
+        public string Message { get; set; }
+        public string Success { get; set; }
+    }
+}
